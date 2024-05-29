808107267b96e033ab2b130e105261fec1988d33
pythonvirus <sachingupta.itm@gmail.com>
2024-05-29 11:33:16 +0400

    "Image generation code update"

--- stablediffusion.py
+++ stablediffusion.py
@@ -2,10 +2,15 @@ import boto3
 import json
 import base64
 import os
+from datetime import datetime
 
 prompt_data="""
 
-provide me an 4k hd image of a beach,also use a blue sky rainy season and cinematic display
+Do not use write title words in the image.
+
+Please make you do not generate add any text while generating the image. Image should have objects but no written text.
+
+Generate image using above guidelines for elearning course title "HR guidelines for employees"
 
 """
 
@@ -32,8 +37,20 @@ response=bedrock.invoke_model(
     contentType="application/json"
 )
 
+
+def timer(start_time=None):
+    if not start_time:
+        start_time = datetime.now()
+        return start_time
+    elif start_time:
+        thour, temp_sec = divmod((datetime.now() - start_time).total_seconds(), 3600)
+        tmin, tsec = divmod(temp_sec, 60)
+        print('\n Time taken: %i hours %i minutes and %s seconds.' % (thour, tmin, round(tsec, 2)))
+
+start_time = timer(None) # timing starts from this point for "start_time" variable
 response_body=json.loads(response.get("body").read())
 print("image generated successfully")
+timer(start_time) 
 #print(response_body)
 artifact = response_body.get("artifacts")[0]
 image_encoded = artifact.get("base64").encode("utf-8")
@@ -47,3 +64,5 @@ with open(file_name, "wb") as f:
     f.write(image_bytes)
 
 print("image saved successfully")
+
+
