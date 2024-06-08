1f60727ada2acd29d260d50f418a4677a70334a5
pythonvirus <sachingupta.itm@gmail.com>
2024-06-08 10:23:40 +0400

    "update requirement"

---
+++ llama2_sunny.py
@@ -0,0 +1,33 @@
+import boto3
+import json
+import os, sys
+
+bedrock=boto3.client(service_name="bedrock-runtime", region_name='us-east-1')
+
+
+model_id="meta.llama2-70b-chat-v1"
+
+#system prompt
+prompt_data="""
+You are a politics expert then tell me how can I win the election 
+"""
+
+payload={
+    "prompt":"[INST]"+prompt_data+"[/INST]",
+    "max_gen_len":512,
+    "temperature":0.5,
+    "top_p":0.9
+}
+
+body=json.dumps(payload)
+
+response=bedrock.invoke_model(
+    body=body,
+    modelId=model_id,
+    accept="application/json",
+    contentType="application/json"
+)
+
+response_body=json.loads(response.get("body").read())
+response_text=response_body['generation']
+print(response_text)
